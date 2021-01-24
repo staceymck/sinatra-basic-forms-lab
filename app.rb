a83109ba5877931ec6cb9f961482ff69dfdc890b
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-24 13:35:05 -0600

    commit tree 88c96d8e4982d8cbd858623c919d558d04428f12

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do #shows home page with link to new puppy form
+    erb :index
+  end
+
+  get '/new' do #presents user with form for new puppy
+    erb :create_puppy
+  end
+
+  post '/puppy' do #shows user the new puppy
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    
+    erb :display_puppy
+  end
+
+
 
 end
